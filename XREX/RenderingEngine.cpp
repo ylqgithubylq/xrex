7d6ce5f7ffbf7f65dab11f90bb8755323be00553
ylqtfosorcimylq@hotmail.com <ylqtfosorcimylq@hotmail.com@deae3b63-d0c6-9bcb-6d87-69a023907217>
2012-11-09 11:07:33 +0000

    Hack patch for CEGUI(restore all pipeline states to default, active texture set to 0, bind buffer 0, before render CEGUI).

--- XREX/RenderingEngine.cpp
+++ XREX/RenderingEngine.cpp
@@ -2,6 +2,8 @@
 
 #include "RenderingEngine.hpp"
 
+#include "XREXContext.hpp"
+#include "RenderingFactory.hpp"
 #include "NaiveManagedScene.hpp"
 #include "Transformation.hpp"
 #include "Renderable.hpp"
@@ -9,6 +11,7 @@
 #include "RenderingEffect.hpp"
 #include "RenderingLayout.hpp"
 #include "Material.hpp"
+#include "RenderingPipelineState.hpp"
 
 #include "GLUtil.hpp"
 #include "DefinedShaderName.hpp"
@@ -21,7 +24,7 @@ namespace XREX
 {
 
 	RenderingEngine::RenderingEngine()
-		: scene_(MakeSP<NaiveManagedScene>())
+		: scene_(MakeSP<NaiveManagedScene>()), defaultBlendColor_(0, 0, 0, 1)
 	{
 	}
 
@@ -42,9 +45,17 @@ namespace XREX
 	void RenderingEngine::Initialize()
 	{
 		// move gl context creation here?
+		//gl::Enable(gl::GL_DEBUG_OUTPUT); // ogl 4.3
+
 		gl::Enable(gl::GL_POLYGON_OFFSET_FILL);
 		gl::Enable(gl::GL_POLYGON_OFFSET_POINT);
 		gl::Enable(gl::GL_POLYGON_OFFSET_LINE);
+		RasterizerState rasterizerState;
+		defaultRasterizerState_ = XREXContext::GetInstance().GetRenderingFactory().CreateRasterizerStateObject(rasterizerState);
+		DepthStencilState depthStencilState;
+		defaultDepthStencilState_ = XREXContext::GetInstance().GetRenderingFactory().CreateDepthStencilStateObject(depthStencilState);
+		BlendState blendState;
+		defaultBlendState_ = XREXContext::GetInstance().GetRenderingFactory().CreateBlendStateObject(blendState);
 	}
 
 	void RenderingEngine::RenderAFrame()
@@ -60,12 +71,31 @@ namespace XREX
 			beforeRenderingFunction_(currentTime, delta);
 		}
 
+#ifdef USE_OPENGL_COMPATIBILITY_PROFILE
+		// TODO temp hack for CEGUI. it seems CEGUI do not disable depth test itself
+		gl::Enable(gl::GL_POLYGON_OFFSET_FILL);
+		gl::Enable(gl::GL_POLYGON_OFFSET_POINT);
+		gl::Enable(gl::GL_POLYGON_OFFSET_LINE);
+#endif
+
 		RenderScene();
+
 #ifdef USE_OPENGL_COMPATIBILITY_PROFILE
 		gl::UseProgram(0);
+		gl::ActiveTexture(gl::GL_TEXTURE0);
+		gl::BindBuffer(gl::GL_ARRAY_BUFFER, 0);
+		gl::BindBuffer(gl::GL_ELEMENT_ARRAY_BUFFER, 0);
 		// TODO temp hack for CEGUI. it seems CEGUI do not disable depth test itself
+		defaultRasterizerState_->Bind(0, 0);
+		defaultDepthStencilState_->Bind(0, 0);
+		defaultBlendState_->Bind(defaultBlendColor_);
+		gl::Disable(gl::GL_POLYGON_OFFSET_FILL);
+		gl::Disable(gl::GL_POLYGON_OFFSET_POINT);
+		gl::Disable(gl::GL_POLYGON_OFFSET_LINE);
 		gl::Disable(gl::GL_DEPTH_TEST);
+		gl::Disable(gl::GL_CULL_FACE);
 #endif
+
 		if (afterRenderingFunction_ != nullptr)
 		{
 			afterRenderingFunction_(currentTime, delta);
