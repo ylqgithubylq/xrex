c4d5766c89e549555f492f12ef181f18006b488f
ylqtfosorcimylq@hotmail.com <ylqtfosorcimylq@hotmail.com@deae3b63-d0c6-9bcb-6d87-69a023907217>
2012-12-07 10:55:45 +0000

    Include a model into svn.

--- XREX/GLUtil.cpp
+++ XREX/GLUtil.cpp
@@ -69,27 +69,7 @@ namespace XREX
 			assert(false);
 			return 0;
 		}
-// 		static std::array<uint32, static_cast<uint32>(ElementType::ParameterTypeCount)> const mapping = [] ()
-// 		{
-// 			std::remove_const<decltype(mapping)>::type parameterMap;
-// 			parameterMap[static_cast<uint32>(ElementType::Bool)] = gl::GL_BOOL;
-// 			parameterMap[static_cast<uint32>(ElementType::Uint8)] = gl::GL_UNSIGNED_BYTE;
-// 			parameterMap[static_cast<uint32>(ElementType::Uint16)] = gl::GL_UNSIGNED_SHORT;
-// 			parameterMap[static_cast<uint32>(ElementType::Uint32)] = gl::GL_UNSIGNED_INT;
-// 			parameterMap[static_cast<uint32>(ElementType::Int8)] = gl::GL_BYTE;
-// 			parameterMap[static_cast<uint32>(ElementType::Int16)] = gl::GL_SHORT;
-// 			parameterMap[static_cast<uint32>(ElementType::Int32)] = gl::GL_INT;
-// 			parameterMap[static_cast<uint32>(ElementType::Float)] = gl::GL_FLOAT;
-// 			parameterMap[static_cast<uint32>(ElementType::FloatV2)] = gl::GL_FLOAT_VEC2;
-// 			parameterMap[static_cast<uint32>(ElementType::FloatV3)] = gl::GL_FLOAT_VEC3;
-// 			parameterMap[static_cast<uint32>(ElementType::FloatV4)] = gl::GL_FLOAT_VEC4;
-// 			parameterMap[static_cast<uint32>(ElementType::Sampler1D)] = gl::GL_SAMPLER_1D;
-// 			parameterMap[static_cast<uint32>(ElementType::Sampler2D)] = gl::GL_SAMPLER_2D;
-// 			parameterMap[static_cast<uint32>(ElementType::Sampler3D)] = gl::GL_SAMPLER_3D;
-// 			parameterMap[static_cast<uint32>(ElementType::SamplerCube)] = gl::GL_SAMPLER_CUBE;
-// 			return parameterMap;
-// 		} ();
-// 		return mapping[static_cast<uint32>(type)];
+
 	}
 
 	uint32 GLPolygonModeFromPolygonMode(RasterizerState::PolygonMode polygonMode)
